fa0949cfacf9fb5b41503acaf3b6ffaa1c6edf93
Deepank Vora <deepankvora@gmail.com>
2024-07-06 12:11:36 +0530

    Initial Commit

---
+++ runpad-github-actions-example-ios/runpad_github_actions_example_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  runpad_github_actions_example_iosApp.swift
+//  runpad-github-actions-example-ios
+//
+//  Created by Deepank Vora on 7/6/24.
+//
+
+import SwiftUI
+
+@main
+struct runpad_github_actions_example_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
